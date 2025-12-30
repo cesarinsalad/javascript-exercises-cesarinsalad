d284955f85f35884913672e1ce4c13c4668846d7
cesarinsalad <cesgarcia1710@gmail.com>
2025-12-30 18:05:16 -0400

    solved exercise 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(text) {
+    let i = text.length - 1;
+    let reversedString = "";
 
+    for (i; i >= 0 ; i--) {
+        reversedString += text[i];
+    }
+
+    return reversedString;
 };
 
 // Do not edit below this line
