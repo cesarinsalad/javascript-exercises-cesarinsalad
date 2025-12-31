4ef0cdd76aadd3e7812c6688f2a71e1951389100
cesarinsalad <cesgarcia1710@gmail.com>
2025-12-31 00:40:32 -0400

    solve exercise 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(numberOne, numberTwo) {
 
+    if ((!Number.isInteger(numberOne)) || (!Number.isInteger(numberTwo))) {
+        return "ERROR";
+    }
+    else if ((numberOne < 0) || (numberTwo < 0)) {
+        return "ERROR";
+    }
+
+    let A = (numberOne < numberTwo) ? numberOne : numberTwo;
+    let B = (numberTwo > numberOne) ? numberTwo : numberOne; 
+    let result = 0;
+    let i = A;
+
+    for (i; i <= B; i++) {
+        result += i;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
